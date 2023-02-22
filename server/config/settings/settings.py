349315dba050fc5c869e6c0059d9b8a5b31c866d
Roman Pavlyuchenko <romanpavlyuchenko@inbox.ru>
2023-02-22 16:58:27 +0300

    docker volumes fix, template fix

--- server/config/settings/settings.py
+++ server/config/settings/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6dr#zo$szouj6)r&gx=@*9e$u+m3o=%etgc*e8gy7=hxqiwm0&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
